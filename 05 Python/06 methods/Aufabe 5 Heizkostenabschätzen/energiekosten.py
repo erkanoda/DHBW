34d93c56d240960ae4e7baf647202b4607b36330
erkanoda <196062437+erkanoda@users.noreply.github.com>
2025-06-04 14:45:47 +0000

    Ordner

--- 05 Python/06 methods/Aufabe 5 Heizkostenabschätzen/energiekosten.py
+++ 05 Python/06 methods/Aufabe 5 Heizkostenabschätzen/energiekosten.py
@@ -1,19 +1,17 @@
 Auswahl = input("Erdgas, Heizöl oder Strom? ")  
 energie = float(input("Geben Sie die Energie in kwh ein: "))
 
-
-def Auswahl():
-    if "Erdgas":
+def konvert(a):
+    if a == "Erdgas":
         tarif = 0.09
-    elif "Heizöl":
+    elif a == "Heizöl":
         tarif = 0.07    
-    elif "Strom":
+    elif a == "Strom":
         tarif = 0.30
-    auswahl = input("Ihre Wahl:")
-    return Auswahl
-
+    return tarif
 
 def energiekosten(energie, tarif):
     kosten = energie * tarif
     return kosten
-print(energiekosten(energie, Auswahl))
+print(energiekosten(energie, konvert(Auswahl))) 
+
